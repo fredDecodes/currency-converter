703c2ef9df1f9aaf040409289824688d837de95a
fred <86912882+fredDecodes@users.noreply.github.com>
2026-05-09 15:50:57 +0000

    Create function to handle event changes

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ const menu1Value = document.getElementById('menu1-value');
 const currencyMenu2El = document.getElementById('currency-menu2');
 const menu2Value = document.getElementById('menu2-value');
 const exchangeRateEl = document.getElementById('exchange-rate');
+
+function updateExchangeRate() {}
