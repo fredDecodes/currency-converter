bc22d8e71d82c6e0625cd23a130261e44f50adc2
fred <86912882+fredDecodes@users.noreply.github.com>
2026-05-09 15:48:32 +0000

    Create variables to hold elements

---
+++ index.js
@@ -0,0 +1,5 @@
+const currencyMenu1El = document.getElementById('currency-menu1');
+const menu1Value = document.getElementById('menu1-value');
+const currencyMenu2El = document.getElementById('currency-menu2');
+const menu2Value = document.getElementById('menu2-value');
+const exchangeRateEl = document.getElementById('exchange-rate');
